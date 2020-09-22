2dab12e56ee384d740d2cef29e02857f4d3c465f
Kiril <kiril@olgas-air.mynet>
2020-09-22 10:53:52 +0100

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#print a list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#print the number of students
 print "Overall, we have "
+#use print to avoid newline chars
 print 11
 puts " great students"
