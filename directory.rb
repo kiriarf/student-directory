a65a0bbcf468430ba4917e0d6e688587291aa225
Kiril <kiril@olgas-air.mynet>
2020-09-22 11:26:33 +0100

    Added conditional based on number of students and refactored the code

--- directory.rb
+++ directory.rb
@@ -1,34 +1,51 @@
-#All students are inside the array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 #print the header
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
+#input student information manually
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    if students.count == 1
+      puts "We have our first student!"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 #print the list of students
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 #print the number of students
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  if students.count == 1
+    puts "We have one great student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end
 end
+
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
