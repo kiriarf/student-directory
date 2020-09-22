38f7f9d7c1491f7b85a96fefb2f266c706ba30a4
Kiril <kiril@olgas-air.mynet>
2020-09-22 12:26:39 +0100

    Moved print_with_letter and print_shorter_than_12 to directory_exercises

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -28,10 +28,33 @@ def input_students
   students
 end
 
-#print the list of students
-def print(students)
+#print the list of students whose name begins with a specific letter
+def print_with_letter(students)
+  #ask for a letter
+  puts "Enter a letter to filter students or hit return to finish"
+  letter = gets.chomp
+  student_number = 1
+  puts "Names starting with #{letter}:"
+  while !letter.empty? do
+    students.each_with_index do |student, index|
+      if student[:name][0] == letter
+        puts "#{student_number}. #{student[:name]} (#{student[:cohort]} cohort)"
+        student_number += 1
+      end
+    end
+    letter = gets.chomp
+  end
+end
+
+#print students whose names are < 12 chars long
+def print_shorter_than_12(students)
+  puts "Names shorter than 12 characters:"
+  student_number = 1
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].length < 12
+      puts "#{student_number}. #{student[:name]} (#{student[:cohort]} cohort)"
+      student_number += 1
+    end
   end
 end
 
@@ -47,5 +70,6 @@ end
 #nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+print_with_letter(students)
+print_shorter_than_12(students)
 print_footer(students)
